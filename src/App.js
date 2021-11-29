409e064a5a58929720839384550d3216a16db686
Prathik Pugazhenthi <prathik.pappu@yahoo.co.in>
2021-11-29 15:53:03 +0530

    Preloader fix

--- src/App.js
+++ src/App.js
@@ -3,8 +3,22 @@ import {BrowserRouter} from "react-router-dom";
 import Main from './Components/Main';
 
 function App() {
+
+  const exitPreloader = () => {
+    var preloader = document.getElementById('preloader');
+    var circle = document.getElementById('circle');
+    preloader.style.display= 'none';
+    circle.style.display = 'none';
+  }
+
   return (
-    <div>
+    <div onLoad={exitPreloader}>
+      <div id="preloader" style={{display:"flex"}}>
+        <div id="circle"></div>
+        <div id="borderCircle1"></div>
+        <div id="borderCircle"></div>
+      </div>
+
       <BrowserRouter>
         <Main/>
       </BrowserRouter>
